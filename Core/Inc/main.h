9884cb1e9cd2b0c609e28570adcc10839de84557
Dennis <dstumm@yandex.ru>
2022-10-23 19:06:58 +0200

    Added base file comment

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -1,3 +1,13 @@
+/**
+ ******************************************************************************
+ * @file main.h
+ * @author Dennis Stumm
+ * @date 2022
+ * @version 1.0
+ * @brief Main header file of the STM32 playground project.
+ ******************************************************************************
+ */
+
 #ifndef INC_MAIN_H_
 #define INC_MAIN_H_
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,3 +1,13 @@
+/**
+ ******************************************************************************
+ * @file main.c
+ * @author Dennis Stumm
+ * @date 2022
+ * @version 1.0
+ * @brief Main file of the STM32 playground project.
+ ******************************************************************************
+ */
+
 #include <main.h>
 
 int main(void) {
